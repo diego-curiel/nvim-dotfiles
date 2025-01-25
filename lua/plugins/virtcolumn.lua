798997bc0ffd8edf4a3c31bf2677923eb85dea14
diego_curiel <177177256+diego-curiel@users.noreply.github.com>
2025-01-25 17:24:44 -0400

    Fix: remove nvim_set_hl function from config
    It wasn't loading correctly

--- lua/plugins/virtcolumn.lua
+++ lua/plugins/virtcolumn.lua
@@ -6,8 +6,6 @@ return{
 		config = function()
 			vim.g.virtcolumn_char = " |" -- char to display as line
 			vim.g.virtcolumn_priority = 10 -- priority of extmark
-			vim.api.nvim_set_hl(0, "VirtColumn",
-								{sp = "#e8e8e8", bg = none})
 		end
 	}
 }
