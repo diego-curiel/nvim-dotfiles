2a08618dc95ef297a4141513a63053accf3afdf4
diego_curiel <177177256+diego-curiel@users.noreply.github.com>
2025-01-25 17:42:12 -0400

    Change the virtcolumn_char value in config

--- lua/plugins/virtcolumn.lua
+++ lua/plugins/virtcolumn.lua
@@ -4,7 +4,7 @@ return{
 		name= "virtcolumn",
 		
 		config = function()
-			vim.g.virtcolumn_char = " |" -- char to display as line
+			vim.g.virtcolumn_char = " ▕" -- char to display as line
 			vim.g.virtcolumn_priority = 10 -- priority of extmark
 		end
 	}
