0aea690c4398aad8de3fad93f4a2a6b0b78c9fdb
diego_curiel <177177256+diego-curiel@users.noreply.github.com>
2025-01-26 21:16:25 -0400

    Remove unnecessary lines and change vim.opt.signcolumn

--- init.lua
+++ init.lua
@@ -1,22 +1,27 @@
-require("config.colorscheme")
---require("config.harpoon")
-require("config.keymaps")
-require("config.lazy")
---require("config.telescope")
---require("config.treesitter")
---require("config.undotree")
---require("config.fugitive")
+require("config.colorscheme") -- Load colorscheme settings
+require("config.keymaps") -- Load personalized keymaps
+require("config.lazy") -- Lazy plugin loader
 
+-- Disable mouse interaction
 vim.opt.mouse = ""
+-- Set number bar type to relative
 vim.opt.relativenumber = true
+-- Set number bar
 vim.opt.number = true
+-- Disable spelling check
 vim.opt.spell = false
-vim.opt.signcolumn = "auto"
+-- Disable word wrapping
 vim.opt.wrap = false
+-- Automatic indentation size
 vim.opt.shiftwidth = 4
+-- Indentation size
 vim.opt.tabstop = 4
+-- VirtColumn position
 vim.opt.cc = "80"
+-- Reserve a space in the gutter
+vim.opt.signcolumn = 'yes'
 
+-- This just a small fix, won't last long
 function ForceVirtColumnHl()
 	vim.api.nvim_set_hl(0, "VirtColumn",
 						{sp = "#e8e8e8", bg = none, force = true})
