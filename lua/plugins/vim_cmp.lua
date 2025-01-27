9fffb1343c96b8472486c0e3bf7683a80a367c6f
diego_curiel <177177256+diego-curiel@users.noreply.github.com>
2025-01-26 23:34:11 -0400

    Add buffer to sources and change some mappings

--- lua/plugins/vim_cmp.lua
+++ lua/plugins/vim_cmp.lua
@@ -3,12 +3,18 @@ return {
 		'hrsh7th/nvim-cmp',
 		name= 'nvim-cmp',
 
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer", 
+		},
+
 		config = function()
 			local cmp = require('cmp')
 
 			cmp.setup({
 				sources = {
 					{name = 'nvim_lsp'},
+					{name = 'buffer'},
 				},
 				snippet = {
 					expand = function(args)
@@ -16,7 +22,18 @@ return {
 						vim.snippet.expand(args.body)
 					end,
 				},
-				mapping = cmp.mapping.preset.insert({}),
+				completion = {
+					autocomplete = false,
+				},
+				mapping = cmp.mapping.preset.insert({
+					-- Confirm autocompletion with enter
+					['<CR>'] = cmp.mapping.confirm({select=false}),
+					['<Tab>'] = cmp.mapping.complete(),
+				}),
+				window = {
+					documentation = cmp.config.window.bordered(),
+					completion = cmp.config.window.bordered(),
+				},
 			})
 		end
 	}
