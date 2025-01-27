169506105c13663e6168d1d6a8ee122ba7bef2bb
diego_curiel <177177256+diego-curiel@users.noreply.github.com>
2025-01-26 21:19:03 -0400

    Fix: remove unnecessary spaces

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,7 @@
 return {{ 
 	'nvim-telescope/telescope.nvim', version = '0.1.8',
 	-- or                            , branch = '0.1.x',
-	dependencies = { {'nvim-lua/plenary.nvim'} },
+	dependencies = { 'nvim-lua/plenary.nvim'},
 
 	config = function()
 		require('telescope').setup({})
