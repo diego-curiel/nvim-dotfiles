f234d17db37a398f7ff9714fe650fc2eff2433ec
diego_curiel <177177256+diego-curiel@users.noreply.github.com>
2025-01-27 03:02:51 -0400

    Add nvim-ufo plugin

---
+++ lua/plugins/nvim_ufo.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		'kevinhwang91/nvim-ufo',
+		name = 'ufo',
+
+		dependencies = {
+			'kevinhwang91/promise-async'
+		},
+
+		config = function()
+			local ufo = require('ufo')
+			local lspconfig = require('lspconfig')
+
+			vim.o.foldcolumn = '1'
+			vim.o.foldlevel = 99
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+
+			vim.keymap.set('n', 'zR', ufo.openAllFolds)
+			vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+
+			local  capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities.textDocument.foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true
+			}
+			local language_servers = lspconfig.util._available_servers()
+			for _,ls in ipairs(language_servers) do
+				lspconfig[ls].setup({
+					capabilities = capabilities
+				})
+			end
+			ufo.setup()
+		end
+	}
+}
