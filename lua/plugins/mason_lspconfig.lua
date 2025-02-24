a4d98a4f700c73ab85a82ff32dbde69783e13f2d
Diego Curiel <177177256+diego-curiel@users.noreply.github.com>
2025-02-24 09:12:42 -0400

    rename plugin file

---
+++ lua/plugins/mason_lspconfig.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		'williamboman/mason-lspconfig.nvim',
+		name = 'mason-lspconfig',
+
+		dependencies = {
+			'williamboman/mason.nvim',
+			'neovim/nvim-lspconfig',
+		},
+
+		config = function()
+			require('mason-lspconfig').setup({
+				ensure_installed = { -- lsp list
+					'ast_grep',
+					'pyright',
+					'clangd',
+					'biome',
+					'emmet_language_server',
+					'html',
+				},
+				handlers = {
+					function(server_name)
+						require('lspconfig')[server_name].setup({})
+					end,
+				},
+			})
+		end
+	}
+}
