20c0a92b43474eb7206e3014e65c9bed5974580e
Diego Curiel <177177256+diego-curiel@users.noreply.github.com>
2025-02-24 09:11:41 -0400

    rename plugin file

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,37 @@
+return {
+	{
+		'hrsh7th/nvim-cmp',
+		name= 'nvim-cmp',
+
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer", 
+		},
+
+		config = function()
+			local cmp = require('cmp')
+
+			cmp.setup({
+				sources = {
+					{name = 'nvim_lsp'},
+					{name = 'buffer'},
+				},
+				snippet = {
+					expand = function(args)
+						-- You need Neovim v0.10 to use vim.snippet
+						vim.snippet.expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					-- Confirm autocompletion with enter
+					['<CR>'] = cmp.mapping.confirm({select=false}),
+					['<C-Space>'] = cmp.mapping.complete(),
+				}),
+				window = {
+					documentation = cmp.config.window.bordered(),
+					completion = cmp.config.window.bordered(),
+				},
+			})
+		end
+	}
+}
