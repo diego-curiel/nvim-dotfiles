5e79419b1d70d23da67d59c6e27fd8101c43bb1c
Diego Curiel <177177256+diego-curiel@users.noreply.github.com>
2025-02-24 09:11:58 -0400

    rename plugin file

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,63 @@
+return {
+	{
+		'neovim/nvim-lspconfig',
+		name = 'lspconfig',
+
+		config = function()
+			-- Add cmp_nvim_lsp capabilities settings to lspconfig
+			-- This should be executed before you configure any language server
+			local lspconfig_defaults = require('lspconfig').util.default_config
+			local lspconfig = require('lspconfig')
+			lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+				'force',
+				lspconfig_defaults.capabilities,
+				require('cmp_nvim_lsp').default_capabilities()
+			)
+			
+			lspconfig.emmet_language_server.setup({
+				filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
+				-- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+				-- **Note:** only the options listed in the table are supported.
+				init_options = {
+					---@type table<string, string>
+					includeLanguages = {},
+					--- @type string[]
+					excludeLanguages = {},
+					--- @type string[]
+					extensionsPath = {},
+					--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+					preferences = {},
+					--- @type boolean Defaults to `true`
+					showAbbreviationSuggestions = true,
+					--- @type "always" | "never" Defaults to `"always"`
+					showExpandedAbbreviation = "always",
+					--- @type boolean Defaults to `false`
+					showSuggestionsAsSnippets = false,
+					--- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+					syntaxProfiles = {},
+					--- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+					variables = {},
+				},
+			})
+			-- This is where you enable features that only work
+			-- if there is a language server active in the file
+			vim.api.nvim_create_autocmd('LspAttach', {
+				desc = 'LSP actions',
+				callback = function(event)
+					local opts = {buffer = event.buf}
+
+					vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+					vim.keymap.set('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+					vim.keymap.set('n', '<leader>ge', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+					vim.keymap.set('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+					vim.keymap.set('n', '<leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+					vim.keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()ucr>', opts)
+					vim.keymap.set('n', '<leader>gh', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+					vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+					vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+					vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+				end,
+			})
+		end
+	}
+}
